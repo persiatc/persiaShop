035b7d8ec9db2ee7103a8207ba9a366db7dd21af
root <yaghoubi@persiatc.com>
2021-10-18 09:39:27 +0330

    section 8 edit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -342,7 +342,7 @@
     <section class="content-header">
       <h1>
 
-        <small>صفحه مدیریت</small>
+        <small> مدیریت</small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="{{ route('home') }}"><i class="fa fa-dashboard"></i> خانه</a></li>
